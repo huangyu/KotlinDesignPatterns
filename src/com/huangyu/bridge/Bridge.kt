0d6e0dbb7c1146cadded96f7cfca5d702b4ab872
huangyu0522 <384539643@qq.com>
2018-03-12 09:57:37 +0800

    Bridge

---
+++ src/com/huangyu/bridge/Bridge.kt
@@ -0,0 +1,39 @@
+package com.huangyu.bridge
+
+interface IPicture {
+	fun display()
+}
+
+class PNG : IPicture {
+	override fun display() {
+		println("PNG")
+	}
+}
+
+class JPG : IPicture {
+	override fun display() {
+		println("JPG")
+	}
+}
+
+abstract class System(val pic: IPicture) {
+	abstract fun showPic()
+}
+
+class Mac : System(pic = JPG()) {
+	override fun showPic() {
+		pic.display()
+	}
+}
+
+class Linux : System(pic = PNG()) {
+	override fun showPic() {
+		pic.display()
+	}
+}
+
+class Window : System(pic = JPG()) {
+	override fun showPic() {
+		pic.display()
+	}
+}
