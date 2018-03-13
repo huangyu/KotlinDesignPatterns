0f8123c5c4c72563e454057f7ba09c8a38ecf2a6
huangyu0522 <384539643@qq.com>
2018-03-13 09:37:38 +0800

    Command

--- src/com/huangyu/command/Command.kt
+++ src/com/huangyu/command/Command.kt
@@ -1,31 +1,28 @@
 package com.huangyu.command
 
-class Command {
-
-	interface ICommand {
-		fun request()
-	}
-
-	class ConcreteCommand : ICommand {
-		val receiver: Receiver = Receiver()
-
-		override fun request() {
-			receiver.action()
-		}
-	}
-
-	class Invoker {
-		var command: ICommand = ConcreteCommand()
-
-		fun call() {
-			command.request()
-		}
-	}
-
-	class Receiver {
-		fun action() {
-			println("Receiver do action")
-		}
-	}
-
+interface ICommand {
+	fun request()
 }
+
+class Command : ICommand {
+	val receiver: Receiver = Receiver()
+
+	override fun request() {
+		receiver.action()
+	}
+}
+
+class Invoker {
+	var command: ICommand = Command()
+
+	fun call() {
+		command.request()
+	}
+}
+
+class Receiver {
+	fun action() {
+		println("Receiver do action")
+	}
+}
+
